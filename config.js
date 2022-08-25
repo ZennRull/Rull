1a530cad5be87eea19d76bfae16e0a2758c0ea7f
ZennRull <112041653+ZennRull@users.noreply.github.com>
2022-08-25 09:34:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ global.prems = ['']
 //=============『 Info Owner 』============== //
 global.nameowner = 'Nama Owner'
 global.numberowner = '6285746030186' // Ganti Jadi Nomor mu
-global.instagram = '-'
-global.github = '-'
-global.dana = '-'
+global.instagram = '-' // Ig Kamu
+global.github = '-' // Github kamu (biarin aja kalau gaada)
+global.dana = '-' //Dana pulsa gopay
 global.pulsa = '-'
 global.gopay = '-'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'Nama Bot'
+global.namebot = 'Nama Bot' // Nama Bot
 global.gc = 'https://t.me/pixelbotwhatsapp'
 global.web = 'https://t.me/pixelbotwhatsapp' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 15K\n2 Minggu 10K\n1 Minggu 5K'
